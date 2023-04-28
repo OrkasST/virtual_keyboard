b64b7ec14280c6848fa8f2054874a32ed210e2e0
OrkasST <egor31@outlook.com>
2023-04-28 21:36:40 +0300

    feat: changeContent function added, some en buttons added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function create({element = 'div',className = '', id = '', content='', isInBody = true}) {
+function create({element = 'div',className = '', id = '', content='', isInBody = false}) {
     console.group('Creating div element...');
     let newDiv = document.createElement(element);
     console.log('newDiv: ', newDiv);
@@ -11,8 +11,8 @@ function create({element = 'div',className = '', id = '', content='', isInBody =
 }
 
 function setRelation(parent, child) {
-    if(typeof parent !== "object") throw new Error("Invalid parent element");
-    if(typeof child !== "object") throw new Error("Invalid child element");
+    if(typeof parent !== "object") throw new Error("Failed to set relation: Invalid parent element");
+    if(typeof child !== "object") throw new Error("Failed to set relation: Invalid child element");
     parent.appendChild(child);
     console.group('Parenting...');
     console.log('child: ', child);
@@ -20,19 +20,43 @@ function setRelation(parent, child) {
     console.groupEnd();
 }
 
+function changeContent (element, newContent = '') {
+    if(typeof element !== "object") throw new Error("Failed to change content: Invalid element");
+    element.innerText = newContent;
+}
+
 let lang = 'en';
 const keys = {
     en: [
-        ['~\n`', 'Backquote'],
-        ['!\n1', 'Digit1']
+        ['~ `', 'Backquote'],
+        ['! 1', 'Digit1'],
+        ['@ 2', 'Digit2'],
+        ['# 3', 'Digit3'],
+        ['$ 4', 'Digit4'],
+        ['% 5', 'Digit5'],
+        ['^ 6', 'Digit6'],
+        ['& 7', 'Digit7'],
+        ['* 8', 'Digit8'],
+        ['( 9', 'Digit9'],
+        [') 0', 'Digit0'],
+        ['_ -', 'Minus'],
+        ['+ =', 'Equal'],
+        ['Backspace', 'Backspace'],
     ],
     ru: []
 };
 
-const display = create({element: 'div', id: 'display'});
-const keyboard = create({element: 'div', className:'keyboard', id: 'board'});
+const display = create({element: 'div', id: 'display', isInBody: true});
+const keyboard = create({element: 'div', className:'keyboard', id: 'board', isInBody: true});
 let elements = keys[lang].map(el => {
-    let button = create({element: 'div', className:'keyboard-button', id: el[1], content: el[0], isInBody: false});
+    let button = create({element: 'div', className:'keyboard-button', id: el[1]});
+    if (el[0].indexOf(' ') >= 0) {
+        el[0].split(' ').forEach((el, i) => {
+            setRelation(button, create({element: 'span', className: i === 0 ? 'upper-symbol' : 'lower-symbol', content: el}));
+        });
+        button.classList.add('multi-symbol');
+    }
+    else changeContent(button, el[0]);
     setRelation(keyboard, button);
     return button;
 });
